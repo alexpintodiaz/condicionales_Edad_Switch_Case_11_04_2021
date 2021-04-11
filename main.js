f2734ade7cf79c99a7ba417b63770b787b7ad987
alexpintodiaz <alexanderp143@hotmail.com>
2021-04-11 13:16:16 -0500

    11_04_2021_Taller_2.2

---
+++ main.js
@@ -0,0 +1,9 @@
+let edad = parseInt(prompt("Ingrese por favor su edad"));
+switch(edad){
+    case 18:
+        alert("Es mayor de edad");
+        break;
+    default:
+        alert("La edad ingresada no cumple con los parámetros");
+        break;
+}
